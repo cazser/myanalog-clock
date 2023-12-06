ee72cf44f5a6ac283a3f78dc09276e8dfddde1f1
cazser <2264712072@qq.com>
2023-12-06 16:38:26 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "widget.h"
+
+#include <QApplication>
+#include "analogclock.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    AnalogClock clock;
+    clock.show();
+    return a.exec();
+}
